b477986071faaadddcdf73dac98e619c9d7df8a2
hachemchrayti <hachemfst@gmail.Com>
2021-03-16 18:02:01 +0100

    Async & Await

--- async/app.js
+++ async/app.js
@@ -1,3 +1,79 @@
+/******************************************************Using Async/Await*************************************************/
+console.log("Début");
+
+// getMember()
+//     .then((member)=>getArticles(member))
+//     .then((articles)=>console.log(articles))
+//     .catch((err)=>console.log(err.message))
+
+async function viewArticles(){
+    try {
+        let member= await getMember()
+        let articles= await getArticles(member)
+        console.log(articles);
+    } catch (error) {
+        console.log(error.message);
+    }
+    
+}
+
+viewArticles();
+
+
+
+function getMember(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+                console.log('Member 1');
+                resolve('Member 1')
+                
+            }, 1500);
+
+    })  
+}
+
+
+function getArticles(member){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve([1,2,3])
+        }, 1500);
+    })
+}
+
+console.log('Fin');
+
+
+
+
+/******************************************************PARALLEL PROMISES*************************************************/
+
+// console.log("Début");
+
+// let p1=  new Promise((resolve,reject)=>{
+
+//     setTimeout(() => {
+//         resolve('Promise 1');
+//     }, 1500);
+// })
+
+// let p2=  new Promise((resolve,reject)=>{
+
+//     setTimeout(() => {
+//         resolve('Promise 2');
+//     }, 3000);
+// })
+
+
+// Promise.all([p1,p2])   //all permet d'attendre qu tous les promises finissent
+//         .then(result=>console.log(result))
+
+
+// Promise.race([p1,p2])   //race fait le retour dès que l'un des Promises finit
+//         .then(result=>console.log(result))
+
+// console.log('Fin');
+
 /******************************************************Async using Promises*************************************************/ 
 
 // console.log("Début");
@@ -67,7 +143,6 @@
 // function getArticles(member){
 //     return new Promise((resolve,reject)=>{
 //         setTimeout(() => {
-//             console.log("okii");
 //             resolve([1,2,3])
 //         }, 1500);
 //     })
@@ -76,30 +151,3 @@
 // console.log('Fin');
 
 
-/******************************************************PARALLEL PROMISES*************************************************/
-
-console.log("Début");
-
-let p1=  new Promise((resolve,reject)=>{
-
-    setTimeout(() => {
-        resolve('Promise 1');
-    }, 1500);
-})
-
-let p2=  new Promise((resolve,reject)=>{
-
-    setTimeout(() => {
-        resolve('Promise 2');
-    }, 3000);
-})
-
-
-Promise.all([p1,p2])   //all permet d'attendre qu tous les promises finissent
-        .then(result=>console.log(result))
-
-
-Promise.race([p1,p2])   //race fait le retour dès que l'un des Promises finit
-        .then(result=>console.log(result))
-
-console.log('Fin');
