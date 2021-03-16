3678b025cc2fb86b6b1c2918d38b9ce8f30d5626
hachemchrayti <hachemfst@gmail.Com>
2021-03-16 15:11:25 +0100

    Gérer les apis avec des paramètre sen GET

--- app.js
+++ app.js
@@ -1,37 +1,53 @@
+const func=require('./functions') 
 const express=require("express")
 var morgan = require("morgan")
 const app=  express();
 
-// app.use((req,res,next)=>{
-//     console.log('URL : '+req.url);
-//     next();
-// })
+const members=[
+    {
+        id:1,
+        name:'Hachem'
+    },
+    {
+        id:2,
+        name:'Ahmed'
+    },
+    {
+        id:3,
+        name:'Dhafer'
+    }
+]
 app.use(morgan("dev"));
 
 
-app.get('/api',(req,res)=>{
-    res.send('Root API');
+//http://localhost:8080/Api/v1/members/1
+app.get('/api/v1/members/:id',(req,res)=>{
 
+    res.json(func.sucess(members[(req.params.id)-1]) );
+});
+
+//http://localhost:8080/Api/v1/members or //http://localhost:8080/Api/v1/members?max=:max
+app.get('/api/v1/members',(req,res)=>{
+    if(req.query.max!=undefined && req.query.max>0) {
+        res.json(func.success(members.slice(0,req.query.max)) );
+    }else if(req.query.max!=undefined){
+        res.json(func.error('Wrong max value') );
+    }else{
+        res.json(func.success(members) );
+    }
+    
 });
 
 
-app.get('/api/v1',(req,res)=>{
-    res.send('Root API v1');
+app.get('/api/v1/members/',(req,res)=>{
 
+    res.json(func.success(members) );
 });
-
-//http://localhost:8080/Api/v1/books/1/hachem  => {"id":"1","name":"hachem"}
-app.get('/api/v1/books/:id/:name',(req,res)=>{
-    res.send(req.params);
-
-});
-
 
 
 app.listen(8080,()=>{
     console.log("Started on port 8080");
 })
-
 
 
 
