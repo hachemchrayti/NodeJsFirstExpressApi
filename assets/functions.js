c4684d08ad4b5b93b6dc2628867c87ee411311c7
hachemchrayti <hachemfst@gmail.Com>
2021-03-16 19:43:41 +0100

    Refaire les apis avec promise-mysql avec async/await

--- assets/functions.js
+++ assets/functions.js
@@ -1,15 +1,29 @@
-
-exports.success=function success(result){
+// Format de réponse en cas de succès
+exports.success = (result) => {
     return {
-        status:'success',
-        result:result
+        status: 'success',
+        result: result
     }
 }
 
-exports.error=function error(message){
+// Format de réponse en cas d'erreur
+exports.error = (message) => {
     return {
-        status:'error',
-        result:message
+        status: 'error',
+        message: message
     }
+}
 
+// Vérifie si l'objet envoyé est une erreur
+exports.isErr = (err) => {
+    return err instanceof Error;
+}
+
+// Envoie le bon format de réponse selon l'objet à envoyer
+exports.checkAndChange = (obj) => {
+    if (this.isErr(obj)) {
+        return this.error(obj.message)
+    } else {
+        return this.success(obj)
+    }
 }
