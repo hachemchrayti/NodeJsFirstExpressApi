74b928bb33026a94b36b9019e2f4a381afa97244
hachemchrayti <hachemfst@gmail.Com>
2021-03-16 14:28:20 +0100

    examples with GET methods with params

---
+++ app.js
@@ -0,0 +1,31 @@
+const express=require("express")
+const app=  express();
+
+
+app.get('/api',(req,res)=>{
+    res.send('Root API');
+
+});
+
+
+app.get('/api/v1',(req,res)=>{
+    res.send('Root API v1');
+
+});
+
+//http://localhost:8080/Api/v1/books/1/hachem  => {"id":"1","name":"hachem"}
+app.get('/api/v1/books/:id/:name',(req,res)=>{
+    res.send(req.params);
+
+});
+
+
+
+app.listen(8080,()=>{
+    console.log("Started on port 8080");
+})
+
+
+
+
+
